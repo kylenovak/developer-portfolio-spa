6f6769967db047cd64e6d169f66f9d4fd6c0936b
Kyle Novak <kyle@novak.codes>
2018-01-27 22:09:20 -0600

    style header

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,12 +8,12 @@ class Navigation extends Component {
     return (
       <nav className="App-navigation">
         <ul className="flexbox-item flex-row">
-          <li><NavLink to="/" activeClassName="active">Home</NavLink></li>
-          <li><NavLink to="/#skills" activeClassName="active">Skills</NavLink></li>
-          <li><NavLink to="/#experience" activeClassName="active">Experience</NavLink></li>
-          <li><NavLink to="/#projects" activeClassName="active">Projects</NavLink></li>
-          <li><NavLink to="/#about" activeClassName="active">About</NavLink></li>
-          <li><NavLink to="/#contact" activeClassName="active">Contact</NavLink></li>
+          <li><NavLink to="/" activeClassName="active"><i class="fas fa-home"></i> Home</NavLink></li>
+          <li><NavLink to="/#skills" activeClassName="active"><i class="fas fa-chart-pie"></i> Skills</NavLink></li>
+          <li><NavLink to="/#experience" activeClassName="active"><i class="fas fa-graduation-cap"></i> Experience</NavLink></li>
+          <li><NavLink to="/#projects" activeClassName="active"><i class="fas fa-briefcase"></i> Projects</NavLink></li>
+          <li><NavLink to="/#about" activeClassName="active"><i class="far fa-question-circle"></i> About</NavLink></li>
+          <li><NavLink to="/#contact" activeClassName="active"><i class="far fa-envelope"></i> Contact</NavLink></li>
         </ul>
       </nav>
     );
